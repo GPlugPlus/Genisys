7d3cbd07a47d228bb1b1d47e96816435e6365d84
MrKiaN <Mrkian.s2014@gmail.com>
2016-12-24 07:24:38 -0800

    Update LoginPacket.php

--- src/pocketmine/network/protocol/LoginPacket.php
+++ src/pocketmine/network/protocol/LoginPacket.php
@@ -45,7 +45,7 @@ class LoginPacket extends DataPacket{
 
 	public function decode(){
 		$this->protocol = $this->getInt();
-		if($this->protocol !== Info::CURRENT_PROTOCOL){
+		if(!in_array($this->protocol, Info::ACCEPTED_PROTOCOLS)){
 			return; //Do not attempt to decode for non-accepted protocols
 		}
 
